30e346e63351118366d60f65e6c0ad8bc95c5c4c
Josueosorio12 <josue_osorio@ucne.edu.do>
2021-11-10 00:51:12 -0400

    Add project files.

---
+++ RegistrodeUsuarios/DAL/Contexto.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistrodeUsuarios.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistrodeUsuarios.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Usuarios> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.EnableSensitiveDataLogging().UseSqlite(@"Data Source = DATA\Usuairios.db");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuarios>().HasData(new Usuarios
+            {
+                Usuarioid = 1,
+                Nombres = "Josue",
+                Apellidos = "Osorio",
+                NombreDeUsuario = "Josue2018",
+                Contrasena = "e1ab9d7f0b137ad16566742ad38863ec42b6d7fba157ef51638e60a4e044bd13"
+                //Contrasena: cortina123
+            });
+        }
+    }
+}
---
+++ RegistrodeUsuarios/Migrations/20211110043042_Inicial.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RegistrodeUsuarios.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Usuarioid = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nombres = table.Column<string>(type: "TEXT", nullable: true),
+                    Apellidos = table.Column<string>(type: "TEXT", nullable: true),
+                    NombreDeUsuario = table.Column<string>(type: "TEXT", nullable: true),
+                    Contrasena = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Usuarioid);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Usuarios",
+                columns: new[] { "Usuarioid", "Apellidos", "Contrasena", "NombreDeUsuario", "Nombres" },
+                values: new object[] { 1, "Osorio", "e1ab9d7f0b137ad16566742ad38863ec42b6d7fba157ef51638e60a4e044bd13", "Josue2018", "Josue" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
